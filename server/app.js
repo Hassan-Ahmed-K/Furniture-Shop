d0e65350428576eabb0fdb456bd18e55f2fa0532
Hassan Ahmed Khan <152582752+Hassan-Ahmed-K@users.noreply.github.com>
2025-02-18 16:52:06 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -36,7 +36,13 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // ROUTES 
